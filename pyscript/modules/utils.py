a663e1f62ab8efa694ba314445f10ba87428b119
Dezito <thomas@sloth.nu>
2024-09-11 20:11:02 +0200

    Merge branch 'master' into develop

--- pyscript/modules/utils.py
+++ pyscript/modules/utils.py
@@ -1,6 +1,7 @@
 __version__ = "1.0.0"
 import functools
 import math
+from pprint import pformat
 
 from logging import getLogger
 BASENAME = f"pyscript.modules.{__name__}"
@@ -16,6 +17,7 @@ def in_between(check, start, end):
     - start (datetime|int|float): The start of the range.
     - end (datetime|int|float): The end of the range.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("in_between")
     #return check <= start < end if check <= end else check <= start or start < end
     return start <= check < end if start <= end else start <= check or check < end
 
@@ -27,6 +29,7 @@ def round_up(n, decimals=0):
     - n (float): The number to round up.
     - decimals (int): The number of decimal places to round to.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("round_up")
     n = max(n, 0.0)
     multiplier = 10 ** decimals
     return float(math.ceil(n * multiplier) / multiplier)
@@ -50,6 +53,7 @@ def average(data):
     
 
 def get_specific_values(values, positive_only = False, negative_only = False):
+    _LOGGER = globals()['_LOGGER'].getChild("get_specific_values")
     return_list = []
     for value in values:
         try:
@@ -99,6 +103,7 @@ def keys_exists(element, *keys):
     - element (dict): The dictionary to check.
     - keys (str): A sequence of keys representing the path to check for existence.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("keys_exists")
     if not isinstance(element, dict):
         raise AttributeError('keys_exists() expects dict as first argument.')
     if len(keys) == 0:
@@ -120,12 +125,134 @@ def has_key(d, path):
     - d (dict): The dictionary to check.
     - path (str): The dot-separated path of keys to check for existence.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("has_key")
     try:
         functools.reduce(lambda x, y: x[y], path.split("."), d)
         return True
     except KeyError:
         return False
+
+def get_dict_value_with_path(d, path):
+    """
+    Retrieves the value from a dictionary based on a given path.
+    Parameters:
+        d (dict): The dictionary to retrieve the value from.
+        path (str): The path to the value in dot notation.
+    Returns:
+        The value from the dictionary at the specified path.
+    """
+    _LOGGER = globals()['_LOGGER'].getChild("get_dict_value_with_path")
     
+    keys = path.split('.')
+    for key in keys:
+        if key.isdigit():
+            key = int(key)
+            d = d[key]
+        elif key in d:
+            d = d[key]
+        else:
+            return
+    return d
+
+def set_dict_value_with_path(d, path, value):
+    """
+    Sets the value of a nested dictionary given a path.
+    Args:
+        d (dict): The dictionary to modify.
+        path (str): The path to the value, using dot notation.
+        value: The value to set.
+    Returns:
+        dict: The modified dictionary.
+    Example:
+        >>> d = {'a': {'b': {'c': 1}}}
+        >>> set_dict_value_with_path(d, 'a.b.c', 2)
+        {'a': {'b': {'c': 2}}}
+    """
+    _LOGGER = globals()['_LOGGER'].getChild("set_dict_value_with_path")
+    
+    keys = path.split('.')
+    current = d
+    for i, key in enumerate(keys):
+        if key.isdigit():
+            key = int(key)
+            if i == len(keys) - 1:
+                current[key] = value
+            else:
+                if key >= len(current):
+                    current.extend([{}] * (key + 1 - len(current)))
+                if not isinstance(current[key], (dict, list)):
+                    current[key] = {}
+                current = current[key]
+        else:
+            if i == len(keys) - 1:
+                current[key] = value
+            else:
+                if key not in current or not isinstance(current[key], (dict, list)):
+                    current[key] = {} if not keys[i + 1].isdigit() else []
+                current = current[key]
+    return d
+
+def delete_dict_key_with_path(d, path):
+    """
+    Deletes a key from a nested dictionary based on the given path.
+    Args:
+        d (dict): The nested dictionary.
+        path (str): The path to the key in dot notation.
+    Returns:
+        dict: The modified dictionary with the key removed.
+    Example:
+        >>> d = {'a': {'b': {'c': 1}}}
+        >>> path = 'a.b.c'
+        >>> delete_dict_key_with_path(d, path)
+        {'a': {'b': {}}}
+    """
+    _LOGGER = globals()['_LOGGER'].getChild("delete_dict_key_with_path")
+    
+    keys = path.split('.')
+    current = d
+
+    for i, key in enumerate(keys[:-1]):  # Iterate up to the second last key
+        if key.isdigit():
+            key = int(key)
+            if isinstance(current, list) and 0 <= key < len(current):
+                current = current[key]
+            else:
+                return d
+        elif isinstance(current, dict) and key in current:
+            current = current[key]
+        else:
+            return d
+
+    last_key = keys[-1]
+    if isinstance(current, dict) and last_key in current:
+        del current[last_key]
+    elif isinstance(current, list) and last_key.isdigit():
+        last_key = int(last_key)
+        if 0 <= last_key < len(current):
+            del current[last_key]
+
+    return d
+
+def rename_dict_keys(d, keys_renaming, remove_old_keys=False):
+    """
+    Renames keys in a dictionary based on a given mapping.
+    Args:
+        d (dict): The dictionary to modify.
+        keys_renaming (dict): A dictionary mapping old keys to new keys.
+        remove_old_keys (bool, optional): Whether to remove the old keys from the dictionary. Defaults to False.
+    Returns:
+        dict: The modified dictionary with renamed keys.
+    """
+    _LOGGER = globals()['_LOGGER'].getChild("rename_dict_keys")
+    
+    for old_path, new_path in keys_renaming.items():
+        old_value = get_dict_value_with_path(d, old_path)
+        if old_value is not None:
+            d = set_dict_value_with_path(d, new_path, old_value)
+            if remove_old_keys:
+                d = delete_dict_key_with_path(d, old_path)
+    return d
+
 def update_keys_recursive(obj, key_mapping):
     """
     Recursively update keys in an object (dictionary or list) that may contain nested dictionaries and lists,
@@ -138,6 +265,7 @@ def update_keys_recursive(obj, key_mapping):
     Returns:
     - bool: True if any updates were made, False otherwise.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("update_keys_recursive")
     updated = False  # Flag to track if any updates are made
 
     if isinstance(obj, dict):
@@ -167,7 +295,7 @@ def compare_dicts_unique_to_dict1(dict1, dict2, path=""):
     - dict2 (dict): The second dictionary to compare against.
     - path (str): The current path, used internally for recursion.
     """
-    _LOGGER = globals()['_LOGGER'].getChild("get_closest_key")
+    _LOGGER = globals()['_LOGGER'].getChild("compare_dicts_unique_to_dict1")
     unique_to_dict1 = {}
 
     for key in dict1:
@@ -186,7 +314,7 @@ def compare_dicts_unique_to_dict1(dict1, dict2, path=""):
 
     return unique_to_dict1
 
-def update_dict_with_new_keys(existing_config, new_config, unique_id_key='name'):
+def update_dict_with_new_keys(existing_config, new_config, unique_id_key='name', check_nested_keys=False):
     """
     Enhanced to handle lists of dictionaries, specifically for configurations like sensors.
     Assumes each dictionary in a list has a unique identifier specified by unique_id_key.
@@ -200,6 +328,7 @@ def update_dict_with_new_keys(existing_config, new_config, unique_id_key='name')
     - updated (bool): True if any updates were made to the existing dictionary, False otherwise.
     - existing_config (dict): The updated dictionary.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("update_dict_with_new_keys")
     updated = False
     for key, value in new_config.items():
         if key not in existing_config:
@@ -207,6 +336,9 @@ def update_dict_with_new_keys(existing_config, new_config, unique_id_key='name')
             updated = True
         elif isinstance(value, dict):
             nested_updated, existing_config[key] = update_dict_with_new_keys(existing_config[key], value, unique_id_key)
+            if check_nested_keys and key in existing_config and existing_config.get(key) != new_config.get(key):
+                existing_config[key] = new_config[key]
+                updated = True
             updated = updated or nested_updated
         elif isinstance(value, list):
             if not isinstance(existing_config[key], list):
@@ -236,5 +368,6 @@ def limit_dict_size(dct, size):
     :param size: The dictionary size.
     :return: A dictionary with a maximum of X key-value pairs.
     """
+    _LOGGER = globals()['_LOGGER'].getChild("limit_dict_size")
     # Convert dictionary items to a list, slice to keep the last 30 items, and convert back to dictionary
     return dict(list(dct.items())[:size])
