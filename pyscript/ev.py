72f654aa57114ad71e724b02c9912bef6e574521
Dezito <thomas@sloth.nu>
2024-10-17 20:11:37 +0200

    Added entity available check to charging loss calculation

--- pyscript/ev.py
+++ pyscript/ev.py
@@ -5984,6 +5984,13 @@ if INITIALIZATION_COMPLETE:
         
         global CONFIG, CHARGING_LOSS_CAR_BEGIN_KWH, CHARGING_LOSS_CAR_BEGIN_BATTERY_LEVEL, CHARGING_LOSS_CHARGER_BEGIN_KWH, CHARGING_LOSS_CHARGING_COMPLETED
 
+        if not is_entity_available(CONFIG['charger']['entity_ids']['kwh_meter_entity_id']) or not is_entity_available(CONFIG['charger']['entity_ids']['status_entity_id']) or not is_entity_available(CONFIG['ev_car']['entity_ids']['charging_limit_entity_id']):
+            if is_calculating_charging_loss():
+                _LOGGER.error(f"Entities not available, stopping charging loss calculation")
+                my_notify(message = f"Entities ikke tilgængelig, stopper ladetab kalkulering", title = f"{TITLE} Fejl", notify_list = CONFIG['notify_list'], admin_only = False, always = True, persistent_notification = True, persistent_notification_id=f"{__name__}_charging_loss_not_available")
+                set_state(f"input_boolean.{__name__}_calculate_charging_loss", "off")
+            return
+        
         try:
             battery_size = CONFIG['ev_car']['battery_size']
             charger_current_kwh = float(get_state(CONFIG['charger']['entity_ids']['kwh_meter_entity_id'], float_type=True, try_history=True, error_state=None))
