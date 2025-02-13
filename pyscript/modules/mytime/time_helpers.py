376b6c2aa09163ad223421636d263537bd44b6fc
Dezito <thomas@sloth.nu>
2025-02-13 18:31:32 +0100

    Add function to add months to a given date in time_helpers module

--- pyscript/modules/mytime/time_helpers.py
+++ pyscript/modules/mytime/time_helpers.py
@@ -153,3 +153,9 @@ def is_day(timestamp = None):
         raise ValueError(f"Could find sunrise or sunset: timestamp: {timestamp} sunrise:{sunrise}, sunset:{sunset}")
 
     return sunrise <= timestamp.replace(tzinfo=None) <= sunset
+
+def add_months(current_date, months_to_add):
+    new_date = datetime.datetime(current_date.year + (current_date.month + months_to_add - 1) // 12,
+                        (current_date.month + months_to_add - 1) % 12 + 1,
+                        current_date.day, current_date.hour, current_date.minute, current_date.second)
+    return new_date
