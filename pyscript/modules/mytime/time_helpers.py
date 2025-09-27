055ef4ea81a48eb3b26e53277cc32a1f8bb58c24
Dezito <thomas@sloth.nu>
2025-09-27 07:57:16 +0200

    fix: enhance datetime handling with error logging and timezone removal in time helper functions

--- pyscript/modules/mytime/time_helpers.py
+++ pyscript/modules/mytime/time_helpers.py
@@ -12,36 +12,92 @@ from logging import getLogger
 BASENAME = f"pyscript.modules.{__name__}"
 _LOGGER = getLogger(BASENAME)
 
+def remove_local_timezone(date_time):
+    try:
+        return date_time.replace(tzinfo=None)
+    except Exception as e:
+        _LOGGER.error(f"Error in remove_local_timezone with date_time {date_time} ({type(date_time)}) error: {e}")
+    return date_time
+
 #Get
 def getSecond(date = None):
-    if not date:
-        date = getTime()
-    return date.second
+    _LOGGER = globals()['_LOGGER'].getChild("getSecond")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getSecond with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).second
+    except Exception as e:
+        _LOGGER.error(f"Error in getSecond with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getSecond with date {date} ({type(date)}) error: {e}")
 
 def getMinute(date = None):
-    if not date:
-        date = getTime()
-    return date.minute
+    _LOGGER = globals()['_LOGGER'].getChild("getMinute")
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getMinute with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).minute
+    except Exception as e:
+        _LOGGER.error(f"Error in getMinute with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getMinute with date {date} ({type(date)}) error: {e}")
 
 def getHour(date = None):
-    if not date:
-        date = getTime()
-    return date.hour
+    _LOGGER = globals()['_LOGGER'].getChild("getHour")
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getHour with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).hour
+    except Exception as e:
+        _LOGGER.error(f"Error in getHour with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getHour with date {date} ({type(date)}) error: {e}")
 
 def getDay(date = None):
-    if not date:
-        date = getTime()
-    return date.day
+    _LOGGER = globals()['_LOGGER'].getChild("getDay")
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getDay with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).day
+    except Exception as e:
+        _LOGGER.error(f"Error in getDay with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getDay with date {date} ({type(date)}) error: {e}")
 
 def getTimeStartOfDay(date = None):
-    if not date:
-        date = getTime()
-    return datetime.datetime(date.year, date.month, date.day, 0, 0, 0, 0)
+    _LOGGER = globals()['_LOGGER'].getChild("getTimeStartOfDay")
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getTimeStartOfDay with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(datetime.datetime(date.year, date.month, date.day, 0, 0, 0, 0))
+    except Exception as e:
+        _LOGGER.error(f"Error in getTimeStartOfDay with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getTimeStartOfDay with date {date} ({type(date)}) error: {e}")
 
 def getTimeEndOfDay(date = None):
-    if not date:
-        date = getTime()
-    return datetime.datetime(date.year, date.month, date.day, 23, 59, 59, 0)
+    _LOGGER = globals()['_LOGGER'].getChild("getTimeEndOfDay")
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getTimeEndOfDay with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(datetime.datetime(date.year, date.month, date.day, 23, 59, 59, 0))
+    except Exception as e:
+        _LOGGER.error(f"Error in getTimeEndOfDay with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getTimeEndOfDay with date {date} ({type(date)}) error: {e}")
 
 def getDayOfWeek(date = None):
     """Get day of week in int value
@@ -51,21 +107,20 @@ def getDayOfWeek(date = None):
         int: ex. SATURDAY = 5
     """
     _LOGGER = globals()['_LOGGER'].getChild("getDayOfWeek")
-    if not date:
-        date = getTime()
-    return date.weekday()
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getDayOfWeek with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).weekday()
+    except Exception as e:
+        _LOGGER.error(f"Error in getDayOfWeek with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getDayOfWeek with date {date} ({type(date)}) error: {e}")
 
 def getDayOfWeekText(date = None, translate = False):
-    """Get day of week text
-    If datetime return OpenHAB DateTime Item day of week text
-    else return current time day of week text
-
-    Args:
-        datetime (org.openhab.core.library.items.DateTimeItem, optional): OpenHAB DateTime Item. Defaults to None.
-
-    Returns:
-        str: Day in text form
-    """
+    _LOGGER = globals()['_LOGGER'].getChild("getDayOfWeekText")
+    
     weekdays = {
         "monday": "mandag",
         "tuesday": "tirsdag",
@@ -75,53 +130,106 @@ def getDayOfWeekText(date = None, translate = False):
         "saturday": "lørdag",
         "sunday": "søndag"
     }
-    if not date:
-        date = getTime()
-    
-    day = calendar.day_name[date.weekday()].lower()
-    
     try:
-        if translate:
-            return weekdays[day]
-    except:
-        pass
-    return day
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getDayOfWeekText with date {date} ({type(date)}) is not datetime")
+        
+        day = calendar.day_name[remove_local_timezone(date).weekday()].lower()
+        
+        try:
+            if translate:
+                return weekdays[day]
+        except:
+            pass
+        return day
+    except Exception as e:
+        _LOGGER.error(f"Error in getDayOfWeekText with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getDayOfWeekText with date {date} ({type(date)}) error: {e}")
 
 def getMonth(date = None):
-    if not date:
-        date = getTime()
-    return date.month
+    _LOGGER = globals()['_LOGGER'].getChild("getMonth")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getMonth with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).month
+    except Exception as e:
+        _LOGGER.error(f"Error in getMonth with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getMonth with date {date} ({type(date)}) error: {e}")
 
 def getMonthFirstDay(date = None):
-    if not date:
-        date = getTime()
-    return datetime.datetime(date.year, date.month, 1, 0, 0, 0, 0)
+    _LOGGER = globals()['_LOGGER'].getChild("getMonthFirstDay")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getMonthFirstDay with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(datetime.datetime(year=date.year, month=date.month, day=1, hour=0, minute=0, second=0, microsecond=0))
+    except Exception as e:
+        _LOGGER.error(f"Error in getMonthFirstDay with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getMonthFirstDay with date {date} ({type(date)}) error: {e}")
 
 def getMonthLastDay(date = None):
-    if not date:
-        date = getTime()
-    date = datetime.datetime(date.year, date.month + 1, 1, 23, 59, 59, 0)
-    return date - datetime.timedelta(days=1)
+    _LOGGER = globals()['_LOGGER'].getChild("getMonthLastDay")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getMonthLastDay with date {date} ({type(date)}) is not datetime")
+        
+        date = remove_local_timezone(datetime.datetime(year=date.year, month=date.month + 1, day=1, hour=23, minute=59, second=59, microsecond=0))
+        return date - datetime.timedelta(days=1)
+    except Exception as e:
+        _LOGGER.error(f"Error in getMonthLastDay with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getMonthLastDay with date {date} ({type(date)}) error: {e}")
 
 def getYear(date = None):
-    if not date:
-        date = getTime()
-    return getTime().year
+    _LOGGER = globals()['_LOGGER'].getChild("getYear")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in getYear with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).year
+    except Exception as e:
+        _LOGGER.error(f"Error in getYear with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in getYear with date {date} ({type(date)}) error: {e}")
 
 def date_to_string(date = None, format = "%m/%d/%Y %H:%M:%S"):
-    if not date:
-        date = getTime()
-    if not isinstance(date, (list, tuple)):
-        date = [date]
-        
-    output = []
-    for d in date:
-        output.append(d.strftime(format))
-    return ", ".join(output)
+    _LOGGER = globals()['_LOGGER'].getChild("date_to_string")
+    
+    try:
+        if date is None:
+            date = getTime()
+            
+        if not isinstance(date, (list, tuple)):
+            date = [date]
+            
+        output = []
+        for d in date:
+            if not isinstance(d, datetime.datetime):
+                raise Exception(f"Error in date_to_string with date {date} ({type(date)}) is not datetime")
+            
+            output.append(d.strftime(format))
+        return ", ".join(output)
+    except Exception as e:
+        _LOGGER.error(f"Error in date_to_string with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in date_to_string with date {date} ({type(date)}) error: {e}")
 
 def toDateTime(date = None):
     try:
-        return dateutil.parser.isoparse(date)
+        dt = dateutil.parser.isoparse(date)
+        return remove_local_timezone(dt)
     except:
         return date
     
@@ -138,24 +246,52 @@ def reset_time_to_hour(date = None):
     Returns:
     datetime: A new datetime object with the minute, second, microsecond set to zero.
     """
-    if not date:
-        date = getTime()
-    return date.replace(minute=0, second=0, microsecond=0)
+    _LOGGER = globals()['_LOGGER'].getChild("reset_time_to_hour")
+    
+    try:
+        if date is None:
+            date = getTime()
+        if not isinstance(date, datetime.datetime):
+            raise Exception(f"Error in reset_time_to_hour with date {date} ({type(date)}) is not datetime")
+        
+        return remove_local_timezone(date).replace(minute=0, second=0, microsecond=0)
+    except Exception as e:
+        _LOGGER.error(f"Error in reset_time_to_hour with date {date} ({type(date)}) error: {e}")
+        raise Exception(f"Error in reset_time_to_hour with date {date} ({type(date)}) error: {e}")
 
 def is_day(timestamp = None):
-    if not timestamp:
-        timestamp = getTime()
+    _LOGGER = globals()['_LOGGER'].getChild("is_day")
+    
+    try:
+        if not timestamp:
+            timestamp = getTime()
+        if not isinstance(timestamp, datetime.datetime):
+            raise Exception(f"Error in is_day with timestamp {timestamp} ({type(timestamp)}) is not datetime")
 
-    sunrise = get_astral_event_date(hass, SUN_EVENT_SUNRISE, timestamp.date()).replace(tzinfo=None)
-    sunset = get_astral_event_date(hass, SUN_EVENT_SUNSET, timestamp.date()).replace(tzinfo=None)
+        sunrise = remove_local_timezone(get_astral_event_date(hass, SUN_EVENT_SUNRISE, timestamp.date()))
+        sunset = remove_local_timezone(get_astral_event_date(hass, SUN_EVENT_SUNSET, timestamp.date()))
 
-    if not sunrise or not sunset:
-        raise ValueError(f"Could find sunrise or sunset: timestamp: {timestamp} sunrise:{sunrise}, sunset:{sunset}")
+        if not sunrise or not sunset:
+            raise ValueError(f"Could find sunrise or sunset: timestamp: {timestamp} sunrise:{sunrise}, sunset:{sunset}")
 
-    return sunrise <= timestamp.replace(tzinfo=None) <= sunset
+        return sunrise <= remove_local_timezone(timestamp) <= sunset
+    except Exception as e:
+        _LOGGER.error(f"Error in is_day with timestamp {timestamp} ({type(timestamp)}) error: {e}")
+        raise Exception(f"Error in is_day with timestamp {timestamp} ({type(timestamp)}) error: {e}")
 
 def add_months(current_date, months_to_add):
-    new_date = datetime.datetime(current_date.year + (current_date.month + months_to_add - 1) // 12,
-                        (current_date.month + months_to_add - 1) % 12 + 1,
-                        current_date.day, current_date.hour, current_date.minute, current_date.second)
-    return new_date
+    _LOGGER = globals()['_LOGGER'].getChild("add_months")
+    
+    try:
+        if current_date is None:
+            current_date = getTime()
+        if not isinstance(current_date, datetime.datetime):
+            raise Exception(f"Error in add_months with current_date {current_date} ({type(current_date)}) is not datetime")
+        
+        new_date = datetime.datetime(current_date.year + (current_date.month + months_to_add - 1) // 12,
+                            (current_date.month + months_to_add - 1) % 12 + 1,
+                            current_date.day, current_date.hour, current_date.minute, current_date.second)
+        return remove_local_timezone(new_date)
+    except Exception as e:
+        _LOGGER.error(f"Error in add_months with current_date {current_date} ({type(current_date)}) months_to_add {months_to_add} error: {e}")
+        raise Exception(f"Error in add_months with current_date {current_date} ({type(current_date)}) months_to_add {months_to_add} error: {e}")
